6fc419a6c9ab215c727923f5cd2f97f3b4071fcb
William Ballard <William.ballard@baltek.us>
2016-01-31 20:01:23 -0500

    Again

--- Skylane/www/js/app.js
+++ Skylane/www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-var app = angular.module('starter', ['ionic, ngMap'])
+var app = angular.module('starter', ['ionic'])
 
 
 .run(function($ionicPlatform) {
@@ -32,7 +32,8 @@ app.config(function($stateProvider,$urlRouterProvider) {
   
   .state('home', {
   url: '/home',
-  templateUrl: 'templates/home.html'
+  templateUrl: 'templates/home.html',
+  controller: 'HomeCtrl'
 
 })
 
